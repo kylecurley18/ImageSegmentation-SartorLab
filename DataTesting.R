30706470618f4f4a13e2506f8d55fad2aa3f7521
Kyle Curley <kpcurley18@gmail.com>
2024-02-01 12:50:48 -0500

    Added cell to match barcode with images

---
+++ DataTesting.R
@@ -0,0 +1,2 @@
+KyleData = read.csv("/home/liftlab/ImageSegmentation/runs/segment/predict17/csv/duckweed_data.csv")
+KassidyData = read.csv("/home/liftlab/mnt/gdrive/RaspberryPi/PlantCV_Data/2023-12-05-result-table-single-value-traits.csv")
